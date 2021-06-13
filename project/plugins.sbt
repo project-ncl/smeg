8f81d66216fae8a65ecae6a492d29d69d21aa594
Alex Creasy <acreasy@redhat.com>
2021-06-13 18:50:17 +0100

    Add unit test

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,4 @@
 libraryDependencies += { "org.scala-sbt" %% "scripted-plugin" % sbtVersion.value }
+
+addSbtPlugin("com.artima.supersafe" % "sbtplugin" % "1.1.12")
+
---
+++ src/test/scala/org/jboss/pnc/smeg/UnitSpec.scala
@@ -0,0 +1,7 @@
+package org.jboss.pnc.smeg
+
+import org.scalatest._
+import flatspec._
+import org.scalatest.matchers.should
+
+abstract class UnitSpec extends AnyFlatSpec with should.Matchers with OptionValues with Inside with Inspectors
