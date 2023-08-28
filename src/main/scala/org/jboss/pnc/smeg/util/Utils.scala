ff3170c7a8a9da3dc8d74f3351fbfa16d9372318
Matej Lazar <matejonnet@gmail.com>
2023-08-28 16:30:30 +0200

    Add OTEL metrics.

--- src/main/scala/org/jboss/pnc/smeg/util/Utils.scala
+++ src/main/scala/org/jboss/pnc/smeg/util/Utils.scala
@@ -1,4 +1,23 @@
 package org.jboss.pnc.smeg.util
 
 object Utils {
+
+  def parseStringToMap(inputString: String): Map[String, String] = {
+    if (inputString == null)
+      return null
+
+    val keyValuePairs = inputString.split(",")
+
+    keyValuePairs.foldLeft(Map.empty[String, String]) { (map, pair) =>
+      val keyValue = pair.split(":")
+      if (keyValue.length == 2) {
+        val key = keyValue(0).trim()
+        val value = keyValue(1).trim()
+        map + (key -> value)
+      } else {
+        map
+      }
+    }
+  }
+
 }
